b6c468968c433a0dc2fb36179608f0c52338fcde
Gerardo Cid <gcid12@gmail.com>
2017-04-13 17:39:06 -0400

    modals by company working

--- app/js/jump.js
+++ app/js/jump.js
@@ -179,10 +179,14 @@ function getData(){
           //PUSHING SUBCATEGORY COMPANIES
           for(var m in subCategory.items){
 
+
+
             var company = subCategory.items[m];
             var companyModal= "modal-"+category.key+'-'+c+'-'+m;
             var companyItem= "item-"+category.key+'-'+c+'-'+m;
             var companyTooltip= "tooltip-"+category.key+'-'+c+'-'+m;
+
+            initIndividualModal(companyModal);
 
             //COMPANY TOOLTIP
             var item=$('<div>')
@@ -212,22 +216,20 @@ function getData(){
             //++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-            var modalTemplate = "<div class='item-modal ui basic modal uno'>" +
-                "<i class='close icon'></i>" +
+            if(company.oss){ oss=" OSS <i class='checked icon'></i>" };
+            var oss=" OSS <i class='checked icon'></i>" ;
+
+            var modalTemplate = "<i class='close icon'></i>"+
                 "<div class='ui icon header'> Header text"+
                 "</div>"+
                 "<div class='content'>"+
                   "<div>" +
-                    "<img class='item-image" src="images/logos/js.png'/>" +
-                  "</div>" +
-                  "<div>" +
-                    "<h2>Javascript</h2>" +
-                    "<p> Javascript is a high-level, dynamic, untyped</p><hr/>" +
-                    "<p>"+
-                      " OSS <i class='check icon'></i>"+
-                      " <i class='twitter icon'></i> | "+
-                      " <i class='github icon'></i> "+
-                    "</p>"+
+                    "<img class='item-image' src='../"+company.logo+"'/>"+
+                  "</div>"+
+                  "<div>"+
+                    "<h2>"+company.productname+"</h2>"+
+                    "<p>"+company.brief+"</p><hr/>"+
+                    "<p>"+oss+"</p>"+
                   "</div>"+
                 "</div>"+
                 "<div class='actions'>"+
@@ -243,87 +245,16 @@ function getData(){
                   "<div class='ui blue ok inverted button small'>"+
                     "<i class='checkmark icon'></i> Visit Website"+
                   "</div>"+
-                "</div>"+
-            "</div>";
+                "</div>";
+
 
 
             //THIS CREATES THE HIDDEN MODAL FOR EACH COMPANY
             var allModals =$('<div>')
+                .addClass("item-modal ui basic modal "+companyModal)
                 .html(modalTemplate);
             $("#allModals").append(allModals);
 
-            var modalContainer =$('<div>')
-                .addClass(companyModal+ " c-tooltiptext")
-                .text(company.productname);
-            $("#companyModals").append(modalContainer);
-
-            //THIS CREATES A USELESS DIV WITH A VALUE , STUPID I KNOW
-            var modalTrigger =$('<div>')
-                .addClass("findme_"+companyModal)
-                .attr("value",companyModal);
-
-            $("."+companyItem).append(modalTrigger);
-
-
-
-            //The image
-            var image2 =$('<img>')
-                .addClass('companyLogo')
-                .attr('src',company.logo);
-            $("."+companyModal).append(image2);
-
-
-            //The text content
-            var content =$('<div>')
-                .addClass(companyTooltip+"_content")
-                .addClass(" content");
-            $("."+companyModal).append(content);
-
-            //var companyContent = "."+companyModal+"_content";
-            var companyContent = "."+companyModal;
-
-            //Twitter
-            var social =$('<i>')
-                .addClass("fa fa-twitter right")
-                .attr("href",company.twitter);
-            $("."+companyModal).append(social);
-
-            //Github Stars
-            var stars =$('<a>')
-                .addClass("label right")
-                .attr("href", company.github)
-                .text(company.ghstars);
-            $("."+companyModal).append(stars);
-
-            //Github
-            var social =$('<i>')
-                .addClass("fa fa-github right")
-                .attr("href", company.twitter);
-            $(stars).append(social);
-
-            //OSS
-
-            if(company.oss){
-              var oss =$('<span>')
-                  .addClass("label right")
-                  .text("OSS");
-              $(companyContent).append(oss);
-            }
-
-            //The Product
-            var productName =$('<h5>')
-                .text(company.productname);
-            $(companyContent).append(productName);
-
-            //The Company Name
-            var companyName =$('<p>')
-                .text(company.company);
-            $(companyContent).append(companyName);
-
-            //Description
-            var description =$('<p>')
-                .text(company.brief);
-            $(companyContent).append(description);
 
           }
 
@@ -341,68 +272,28 @@ getData();
 
 
 $(document).ready(function(){
-  //sidebar
-  $("#sidebarTrigger").click(function(){
-    $('.ui.sidebar')
-        .sidebar('setting', 'transition', 'overlay')
-        .sidebar('toggle')
-    ;
-
-  });
-
-  //modal
-  // $("#trigger1").click(function(){
-  //   $('.ui.basic.modal')
-  //       .modal('show');
-  //       blurrr();
-  //
-  // });
-
-
-
-  $("#uno").click(function(){
-      $('.ui.basic.modal.uno').modal('setting',{
-        onHide: function(){
-          console.log('hidden');
-          unblurrr();
-        },
-        onShow: function(){
-          console.log('shown');
-          blurrr();
-        }
-      }).modal('show');
-  });
-
-  $("#dos").click(function(){
-    $('.ui.basic.modal.dos').modal('setting',{
-      onHide: function(){
-        console.log('hidden');
-        unblurrr();
-      },
-      onShow: function(){
-        console.log('shown');
-        blurrr();
-      }
-    }).modal('show');
-  });
-
-  $("#tres").click(function(){
-    $('.ui.basic.modal.tres').modal('setting',{
-      onHide: function(){
-        console.log('hidden');
-        unblurrr();
-      },
-      onShow: function(){
-        console.log('shown');
-        blurrr();
-      }
-    }).modal('show');
-  });
-
-
-
+//Sketchy things happen here
 
 });
+
+
+function initIndividualModal(id){
+
+  $("#"+id).click(function(){
+    $(".ui.basic.modal."+id).modal('setting',{
+      onHide: function(){
+        console.log('hidden');
+        unblurrr();
+      },
+      onShow: function(){
+        console.log('shown');
+        blurrr();
+      }
+    }).modal('show');
+  });
+
+}
+
 
 function blurrr(){
   $(".module").toggleClass("outfocus");
