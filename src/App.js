3432dc04b853956f0d0e8ced965598c159bfa9db
gcid12 <gcid12@gmail.com>
2017-08-11 18:25:39 -0700

    Install Semantic UI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Card } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Card>
+            This is Semantic
+        </Card>
       </div>
     );
   }
