35d3c01546e531e873cb16e21085707c244dda93
gcid12 <gcid12@gmail.com>
2017-09-06 16:54:18 -0700

    Add react-router components

--- src/views/Layout/index.js
+++ src/views/Layout/index.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import { Icon, Menu } from 'semantic-ui-react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import classNames from 'classnames'
 import Header from './../../components/header';
 import SideBar from './../../components/SideBar';
@@ -34,6 +40,19 @@ class Layout extends Component {
   
     this.updateState();
     
+  }
+  
+  previousLocation = this.props.location;
+  
+  componentWillUpdate(nextProps) {
+    const { location } = this.props
+    // set previousLocation if props.location is not modal
+    if (
+      nextProps.history.action !== 'POP' &&
+      (!location.state || !location.state.modal)
+    ) {
+      this.previousLocation = this.props.location
+    }
   }
   
   updateState(){
@@ -82,7 +101,7 @@ class Layout extends Component {
     
   }
   
-  render() {
+  renderdos() {
     
     const data= this.state.landscape;
 
@@ -111,7 +130,139 @@ class Layout extends Component {
     
     );
   }
+  
+  render() {
+    const { location } = this.props
+    const isModal = !!(
+      location.state &&
+      location.state.modal &&
+      this.previousLocation !== location // not initial render
+    )
+    return (
+      <div>
+        <Switch location={isModal ? this.previousLocation : location}>
+          <Route exact path='/' component={Home}/>
+          <Route path='/gallery' component={Gallery}/>
+          <Route path='/img/:id' component={ImageView}/>
+        </Switch>
+        {isModal ? <Route path='/img/:id' component={Modal} /> : null}
+      </div>
+    )
+  }
+
 }
 
 
-export default Layout;
+const IMAGES = [
+  { id: 0, title: 'Dark Orchid', color: 'DarkOrchid' },
+  { id: 1, title: 'Lime Green', color: 'LimeGreen' },
+  { id: 2, title: 'Tomato', color: 'Tomato' },
+  { id: 3, title: 'Seven Ate Nine', color: '#789' },
+  { id: 4, title: 'Crimson', color: 'Crimson' }
+]
+
+const Thumbnail = ({ color }) =>
+  <div style={{
+    width: 50,
+    height: 50,
+    background: color
+  }}/>
+
+const Image = ({ color }) =>
+  <div style={{
+    width: '100%',
+    height: 400,
+    background: color
+  }}/>
+
+const Home = () => (
+  <div>
+    <Link to='/gallery'>Visit the Gallery</Link>
+    <h2>Featured Images</h2>
+    <ul>
+      <li><Link to='/img/2'>Tomato</Link></li>
+      <li><Link to='/img/4'>Crimson</Link></li>
+    </ul>
+  </div>
+)
+
+const Gallery = () => (
+  <div>
+    {IMAGES.map(i => (
+      <Link
+        key={i.id}
+        to={{
+          pathname: `/img/${i.id}`,
+          // this is the trick!
+          state: { modal: true }
+        }}
+      >
+        <Thumbnail color={i.color} />
+        <p>{i.title}</p>
+      </Link>
+    ))}
+  </div>
+)
+
+const ImageView = ({ match }) => {
+  const image = IMAGES[parseInt(match.params.id, 10)]
+  if (!image) {
+    return <div>Image not found</div>
+  }
+  
+  return (
+    <div>
+      <h1>{image.title}</h1>
+      <Image color={image.color} />
+    </div>
+  )
+}
+
+const Modal = ({ match, history }) => {
+  const image = IMAGES[parseInt(match.params.id, 10)]
+  if (!image) {
+    return null
+  }
+  const back = (e) => {
+    e.stopPropagation()
+    history.goBack()
+  }
+  return (
+    <div
+      onClick={back}
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        background: 'rgba(0, 0, 0, 0.15)'
+      }}
+    >
+      <div className='modal' style={{
+        position: 'absolute',
+        background: '#fff',
+        top: 25,
+        left: '10%',
+        right: '10%',
+        padding: 15,
+        border: '2px solid #444'
+      }}>
+        <h1>{image.title}</h1>
+        <Image color={image.color} />
+        <button type='button' onClick={back}>
+          Close
+        </button>
+      </div>
+    </div>
+  )
+}
+
+const ModalGallery = () => (
+  <Router>
+    <Route component={Layout} />
+  </Router>
+)
+
+
+export default ModalGallery;
