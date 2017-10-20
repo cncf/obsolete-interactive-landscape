b301dc4c335971f3d547e3d0828141df50e6b65a
gcid12 <gcid12@gmail.com>
2017-10-20 03:39:35 -0700

    Create independent Category Component

--- src/views/Layout/Layout.js
+++ src/views/Layout/Layout.js
@@ -251,57 +251,6 @@ const Gallery = () => (
   </div>
 );
 
-
-const CategoryViewX = ({ cat }) => {
-  const category = CATEGORIES[cat];
-  const subCategories = category.subcategories;
-  let color = Color(cat);
-  
-  if (!category) {
-    return <div>No Category called like that</div>
-  }
-  
-  return (
-    <div className="module">
-      <Iconator icon={category.slug_name} size="background"/>
-      <div className="stillbox" id={Color(category.slug_name)}>
-        <div className="box-2 category-box">
-          <h2 className="category-title categ-big">
-            <Iconator icon={category.slug_name} size="m"/>
-            
-            <br/>
-            {category.name}
-          </h2>
-          <p className="categ-brief">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam.
-          </p>
-          <div className="companies">
-            <div className=" box-items">
-              {subCategories.map((i,index) => (
-                <Link
-                  key={i.id}
-                  to={{
-                    pathname: `/${category.slug_name}/${index}`,
-                    // this is the trick!
-                    state: { modal: true }
-                  }}
-                >
-                  <h5 className="subcateg">
-                    <i className="caret right icon"/>{i.name}
-                  </h5>
-                </Link>
-              ))}
-            </div>
-            
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-};
-
 const SubCategoryView = ({ cat, match }) => {
   const category = dataSet.landscape[cat];
   const subCategory = category.subcategories[match.params.id];
